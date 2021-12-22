089c7d8232db69c2c7ca046b2dfb84942f8e5b86
chaneho <changho@email.com>
2021-12-22 17:37:19 +0900

    f

--- springEx/src/main/java/org/zerock/controller/SampleController.java
+++ springEx/src/main/java/org/zerock/controller/SampleController.java
@@ -2,12 +2,21 @@ package org.zerock.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -15,6 +24,12 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
+
+//	@InitBinder
+//	public void initBinder(WebDataBinder binder) {
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(sdf, false));
+//	}
 
 	@RequestMapping("")
 	public void basic() {
@@ -54,6 +69,71 @@ public class SampleController {
 	public String ex02Array(@RequestParam("ids") String[] ids) {
 		log.info("ids: " + ids);
 		return "/ex02Array";
+	}
+
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+
+		log.info(list);
+
+		return "/ex02Bean";
+	}
+
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo: " + todo);
+		return "/ex03";
+	}
+
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		log.info("dto: " + dto);
+		log.info("page: " + page);
+
+		return "/sample/ex04";
+	}
+
+	@GetMapping("/ex05")
+	public void ex05() {
+		log.info("ex05...........");
+	}
+
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("ex06");
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+		return dto;
+
+	}
+
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07() {
+		log.info("ex07...");
+		// {"name":"홍길동"}
+		String msg = "{\"name\":\"홍길동\"}";
+
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-type", "application/json;charset=UTF-8");
+
+		return new ResponseEntity<String>(msg, header, HttpStatus.OK);
+	}
+
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		log.info("/exUpload ....");
+	}
+
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		log.info(files);
+
+		files.forEach(file -> {
+			log.info("------------------------");
+			log.info("name: " + file.getOriginalFilename());
+			log.info("size: " + file.getSize());
+		});
 	}
 
 }
